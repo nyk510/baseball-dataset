63728f160c1889c89205057370e67489e142e790
nyker510 <takamail53@gmail.com>
2018-05-24 23:01:54 +0900

    [update] module の api 変更に伴う修正など

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import npbdata as npb
 import os
+
+import npbdata as npb
 
 if __name__ == '__main__':
     root_path = os.getcwd()
@@ -12,16 +13,16 @@ if __name__ == '__main__':
         else:
             print('False')
 
-    df_info = npb.fetch_playerinfo(verbose='INFO')
-    df_info.to_csv('data/player_info.csv',index=False,encoding='utf-8')
+    df_info = npb.fetch_player_info(verbose='INFO')
+    df_info.to_csv('data/player_info.csv', index=False, encoding='utf-8')
 
-    st_years = range(2009,2017)
+    st_years = range(2009, 2017)
     for st_year in st_years:
-        for st_type in ['pitcher','hitter']:
-            df_stats = npb.fetch_stats(year = st_year,stats_type=st_type)
-            df_stats.to_csv('data/{0}_stats_{1}.csv'.format(st_type,st_year),index=False,encoding='utf-8')
+        for st_type in ['pitcher', 'hitter']:
+            df_stats = npb.fetch_stats(year=st_year, stats_type=st_type)
+            df_stats.to_csv('data/{0}_stats_{1}.csv'.format(st_type, st_year), index=False, encoding='utf-8')
 
     start = 2009
     end = 2015
-    df_match = npb.fetch_matchdata(start=start,end=end)
-    df_match.to_csv('data/match_data_from{0}-to{1}.csv'.format(start,end),index=False,encoding='utf-8')
+    df_match = npb.fetch_all_matches(start=start, end=end)
+    df_match.to_csv('data/match_data_from{0}-to{1}.csv'.format(start, end), index=False, encoding='utf-8')
